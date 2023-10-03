7d543d74397c80d9419778b240a1baaa720ea6e4
rcupull <59990075+rcupull@users.noreply.github.com>
2023-10-03 11:35:16 -0400

    fixing linting issues

--- src/Marlin/assets/i18n/index.js
+++ src/Marlin/assets/i18n/index.js
@@ -6,64 +6,64 @@ var dataEn = require('./en.json');
 const translate = require('google-translate-open-api');
 
 async function translateLang(lang = 'en') {
-    var dataLangJSON = DataLang(lang);
-    let total = Object.keys(dataLangJSON).length;
-    errors = [];
-    let i = 0;
+  var dataLangJSON = DataLang(lang);
+  let total = Object.keys(dataLangJSON).length;
+  errors = [];
+  let i = 0;
 
-    for (let key in dataLangJSON) {
-        try {
-            if (!dataLangJSON[key]) {
-                let result = await translate.default(key, {
-                    to: lang,
-                })
-                await waits(100);
-                const data = result.data[0];
-                dataLangJSON[key] = data;
-                console.log(i, '/', total, ' : ', Math.floor((i / total) * 100), '%');
-            }
-            i++;
-        } catch (err) {
-            if (err) {
-                errors.push({
-                    item: key,
-                    code: err.response.status
-                })
-                await waits(500);
-                i++;
-                console.log("********Error traduciendo: ", key, ' status: ', err.response.status);
-            } else {
-                console.log("********Error desconocido: ",);
-            }
-        }
+  for (let key in dataLangJSON) {
+    try {
+      if (!dataLangJSON[key]) {
+        let result = await translate.default(key, {
+          to: lang,
+        });
+        await waits(100);
+        const data = result.data[0];
+        dataLangJSON[key] = data;
+        console.log(i, '/', total, ' : ', Math.floor((i / total) * 100), '%');
+      }
+      i++;
+    } catch (err) {
+      if (err) {
+        errors.push({
+          item: key,
+          code: err.response.status,
+        });
+        await waits(500);
+        i++;
+        console.log('********Error traduciendo: ', key, ' status: ', err.response.status);
+      } else {
+        console.log('********Error desconocido: ');
+      }
     }
+  }
 
-    fs.writeFileSync(`./${lang}.json`, JSON.stringify(dataLangJSON, null, 2));
-    console.log("Terminado todo el proceso de traducción");
-    console.log("Resumen: ");
-    console.log("Errores: ", errors.length);
-    for (let i = 0; i < errors.length; i++) {
-        console.log("Error al traducir ", errors[i].item, ' code:', errors[i].code);
-    }
+  fs.writeFileSync(`./${lang}.json`, JSON.stringify(dataLangJSON, null, 2));
+  console.log('Terminado todo el proceso de traducción');
+  console.log('Resumen: ');
+  console.log('Errores: ', errors.length);
+  for (let i = 0; i < errors.length; i++) {
+    console.log('Error al traducir ', errors[i].item, ' code:', errors[i].code);
+  }
 }
 
 function DataLang(lang = 'en') {
-    switch (lang) {
-        case 'en':
-            return dataEn;
-        case 'es':
-            return dataEs;
-        default:
-            throw new Error('No cumple con los idiomas permitidos, los valores validos son, es, en y fr')
-    }
+  switch (lang) {
+    case 'en':
+      return dataEn;
+    case 'es':
+      return dataEs;
+    default:
+      throw new Error('No cumple con los idiomas permitidos, los valores validos son, es, en y fr');
+  }
 }
 
 function waits(delay = 1) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            return resolve(true);
-        }, delay);
-    })
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve(true);
+    }, delay);
+  });
 }
 
 const lang = process.argv[2];
